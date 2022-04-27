5378b999f22380341b186c32c7fa218cfc04951c
Max Schulz <max.schulz10@web.de>
2022-04-27 17:06:12 +0200

    Added commented remapping, applying montage and rereferencing.

--- EEG_analysis.py
+++ EEG_analysis.py
@@ -14,7 +14,6 @@ import pathlib
 DIR = pathlib.Path.cwd()
 eeg_DIR = DIR / "data"
 
-# load raw data
 raw = mne.io.read_raw_brainvision(eeg_DIR / '20220302_pilot_4l8g2m_trial_1.vhdr', preload=True)
 
 # plot  and inspect raw data. Are there any data segments that
@@ -30,6 +29,45 @@ raw.filter(0.01, 40)
 # Widmann et al. (2015): Digital filter design for electrophysiological data – a practical approach.
 
 raw.plot()  # plot data again, can you see the difference?
+
+# If you look into the channel names you only see number when instead the EEG setup
+# uses electrode names that give information about their position on the scalp
+# (Cz, TP10, ...).
+
+raw.info["chs"]
+
+# In order to change the mapping name, we need a dictionary that
+# transforms the numbers 1 - 64 into corresponding naming conventions.
+
+mapping = {"1": "Fp1", "2": "Fp2", "3": "F7", "4": "F3", "5": "Fz", "6": "F4",
+           "7": "F8", "8": "FC5", "9": "FC1", "10": "FC2", "11": "FC6",
+           "12": "T7", "13": "C3", "14": "Cz", "15": "C4", "16": "T8", "17": "TP9",
+           "18": "CP5", "19": "CP1", "20": "CP2", "21": "CP6", "22": "TP10",
+           "23": "P7", "24": "P3", "25": "Pz", "26": "P4", "27": "P8", "28": "PO9",
+           "29": "O1", "30": "Oz", "31": "O2", "32": "PO10", "33": "AF7", "34": "AF3",
+           "35": "AF4", "36": "AF8", "37": "F5", "38": "F1", "39": "F2", "40": "F6",
+           "41": "FT9", "42": "FT7", "43": "FC3", "44": "FC4", "45": "FT8", "46": "FT10",
+           "47": "C5", "48": "C1", "49": "C2", "50": "C6", "51": "TP7", "52": "CP3",
+           "53": "CPz", "54": "CP4", "55": "TP8", "56": "P5", "57": "P1", "58": "P2",
+           "59": "P6", "60": "PO7", "61": "PO3", "62": "POz", "63": "PO4", "64": "PO8"}
+# We can then use the mapping variable together with the rename_channels function
+# to rename all the channels.
+
+raw.rename_channels(mapping)
+
+# Look at the channel names again, can you see the difference?
+
+raw.info["chs"]
+
+# We further need the information of relative electrodes positions on the scalp to
+# plot topographies and sensor positions on the scalp. The BrainVision company has
+# their own file which contains the exact coordinates for their EEG setup, so we
+# load that into the montage variable and set the montage into our Raw Object.
+
+montage_path = DIR / "AS-96_REF.bvef"
+montage = mne.channels.read_custom_montage(fname=montage_path)
+raw.set_montage(montage)
+
 
 # From the raw data we are going to get the 'events' which are the time points
 # at which a stimulus trigger was recorded.
@@ -53,7 +91,28 @@ epochs = mne.Epochs(raw, events, event_id, tmin, tmax, reject=reject_criteria, f
 
 # We can now average over the epochs to obtain the ERP (= event-related potential).
 # This is what we will work with in the next section.
-epochs.average().plot()
+# Select certain intervals in the joint plot and look at the topography. Does it look
+# as expected? Where do we expect electric activity on the scalp to be highest for
+# auditory evoked potentials?
+
+epochs.average().plot_joint()
+
+# We can plot the sensors ("topomap" or "3d") and decide on what electrodes we want
+# to take as reference. We then select these as the reference or simply take an average
+# of all electrodes activity as reference. You decide, which reference would in
+# theory make most sense?
+
+epochs.plot_sensors(kind="topomap", ch_type='all')
+reference = ["TP10", "TP9"]
+
+# After decision on which reference to choose, we can simply set and apply the
+# reference.
+
+epochs.set_eeg_reference(ref_channels=reference)
+
+# Look at the ERP again: does the response look all right now, and how do you know?
+
+epochs.average().plot_joint()
 
 # However, this does not look right. There are big differences in the channels' baseline
 # which overshadows the actual ERP. We can circumvent this by setting a channel-
